e9d4563061680a7fc086dc382a6eecb2e46d834a
mhweaver <mhweaver@gmail.com>
2016-05-25 14:02:03 -0600

    Implemented basic memory usage and sysload segments

---
+++ avg_load.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	linuxproc "github.com/c9s/goprocinfo/linux"
+	"log"
+	"runtime"
+	"strconv"
+	"time"
+)
+
+type avgLoadSegment struct {
+	Segment
+	output chan string
+	numCpu int
+}
+
+func newAvgLoadSegment() (segment *avgLoadSegment) {
+	segment = new(avgLoadSegment)
+	segment.output = make(chan string)
+	segment.numCpu = runtime.NumCPU()
+	return
+}
+
+func (segment *avgLoadSegment) GetOutputBuffer() chan string {
+	return segment.output
+}
+
+func (segment *avgLoadSegment) Run() {
+	for {
+		loadAvg, err := linuxproc.ReadLoadAvg("/proc/loadavg")
+		if err != nil {
+			log.Fatal("unable to read /proc/loadavg")
+		}
+
+		segment.output <- segment.renderOutput(loadAvg)
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func (segment *avgLoadSegment) renderOutput(loadAvg *linuxproc.LoadAvg) string {
+	yellowThreshold := float64(segment.numCpu)
+	redThreshold := float64(segment.numCpu * 2)
+
+	return " " + renderSingleLoad(loadAvg.Last1Min, yellowThreshold, redThreshold) +
+		" " + renderSingleLoad(loadAvg.Last5Min, yellowThreshold, redThreshold) +
+		" " + renderSingleLoad(loadAvg.Last15Min, yellowThreshold, redThreshold)
+}
+
+func renderSingleLoad(load, yellowThreshold, redThreshold float64) string {
+	var color string
+	switch {
+	case load >= redThreshold:
+		color = "#ff0000"
+	case load >= yellowThreshold:
+		color = "#ffff00"
+	default:
+		color = "#00ff00"
+	}
+	return "%{F" + color + "}" + strconv.FormatFloat(load, 'f', 2, 64) + "%{F-}"
+}
--- mem.go
+++ mem.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	linuxproc "github.com/c9s/goprocinfo/linux"
+	"log"
+	"strconv"
+	"time"
+)
+
 type memSegment struct {
 	Segment
 	output chan string
@@ -16,4 +23,40 @@ func (segment *memSegment) GetOutputBuffer() chan string {
 }
 
 func (segment *memSegment) Run() {
+	for {
+		segment.output <- renderOutput(getMemInfo())
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func renderOutput(free, used, total int64) string {
+	percentUsed := 100 * float64(used) / float64(total)
+	var color string
+	switch {
+	case percentUsed > 95:
+		color = "#ff0000"
+	case percentUsed > 90:
+		color = "#ffae00"
+	case percentUsed > 20:
+		color = "#fff600"
+	default:
+		color = "#ffffff"
+	}
+
+	return " " + "%{F" + color + "}" +
+		strconv.FormatFloat(float64(used)/1024/1024, 'f', 2, 64) +
+		"GiB%{F-} / " + strconv.FormatFloat(float64(total)/1024/1024, 'f', 2, 64) +
+		"GiB (%{F" + color + "}" + strconv.FormatFloat(percentUsed, 'f', 2, 64) + "%%{F-})"
+}
+
+func getMemInfo() (free, used, total int64) {
+	memInfo, err := linuxproc.ReadMemInfo("/proc/meminfo")
+	if err != nil {
+		log.Println("failed to read /proc/meminfo")
+	}
+
+	total = int64(memInfo.MemTotal)
+	free = int64(memInfo.MemFree + memInfo.Buffers + memInfo.Cached)
+	used = int64(total - free)
+	return
 }
